d72deb4ea2670ab29fc7b0e71f704459f3c728b3
Steve NGOYA <maximusdevcode@gmail.com>
2025-04-06 07:03:04 +0100

    modified:   src/App.css 	modified:   src/App.js 	modified:   src/components/Sidebar.js

---
+++ src/components/Sidebar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <ul>
+        <li><a href="#Dashboard">Dashboard</a></li>
+        <li><a href="#Settings">Settings</a></li>
+        <li><a href="#profile">Profile</a></li>
+      </ul>
+    </div>
+  );
+}
+
+export default Sidebar;
